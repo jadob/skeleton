7e4adc588dc867c80eb80e9402fbf125fd66212d
pizzaminded <pizzaminded@users.noreply.github.com>
2025-11-08 01:50:09 +0100

    chore: bump required php version

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use Jadob\Framework\Application;
 use Symfony\Component\HttpFoundation\Request;
 
-if (\version_compare(PHP_VERSION, '8.0.0') < 0) {
+if (\version_compare(PHP_VERSION, '8.4.0') < 0) {
     die('Sorry! Jadob need at least PHP 8.0 to work properly.');
 }
 
