6f973125a1c645ab02e490945b572e4fd5a5057d
pizzaminded <pizzaminded@users.noreply.github.com>
2025-11-08 01:36:30 +0100

    refactor: convert router config to object-oriented

--- config/router.php
+++ config/router.php
@@ -1,5 +1,11 @@
 <?php
 
-return [
-    'routes' => include __DIR__.'/routes.php'
-];
+use Jadob\Router\ServiceProvider\RouterConfiguration;
+
+return function (RouterConfiguration $configuration) {
+    $configuration->importRoutes(include __DIR__ . '/routes.php');
+    $configuration->configureFromBaseUrl($_ENV['BASE_URL']);
+
+
+    return $configuration;
+};
