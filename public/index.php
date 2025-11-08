bd2a29ab03442499c8cf2c4967aae4c743985bac
pizzaminded <pizzaminded@users.noreply.github.com>
2025-11-08 01:54:16 +0100

    refactor: use boot file

--- public/index.php
+++ public/index.php
@@ -6,19 +6,10 @@ use Symfony\Component\HttpFoundation\Request;
 if (\version_compare(PHP_VERSION, '8.4.0') < 0) {
     die('Sorry! Jadob need at least PHP 8.0 to work properly.');
 }
+$app = include __DIR__ . '/../boot.php';
 
-require_once __DIR__.'/../boot.php';
-
-
-$bootstrap = new Bootstrap();
 $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
-$app = new Application(
-    $_ENV['APP_ENV'],
-    new Bootstrap(),
-    [],
-    []
-);
 $app->handleWebRequest($request)->send();
 $app->terminate();
 
